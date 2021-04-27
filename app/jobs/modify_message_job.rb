2e76410512ae2b74617d84d9de8c984bae633962
Aram <aramhrptn@hotmail.com>
2021-04-27 18:26:11 +0700

    fixed error on .sample

--- app/jobs/modify_message_job.rb
+++ app/jobs/modify_message_job.rb
@@ -2,8 +2,9 @@
 
 class ModifyMessageJob < ApplicationJob
   queue_as :default
-  @fail_replies = ['ты че ебанутый?', 'сам сказал что понял?', 'тупа не осилил регулярки',
-                   'тебе с такими регулярками не стыдно людям в глаза смотреть?']
+
+  def fail_replies = ['ты че ебанутый?', 'сам сказал что понял?', 'тупа не осилил регулярки',
+                      'тебе с такими регулярками не стыдно людям в глаза смотреть?']
 
   def perform(_initial_message_id, send_to, initial_text, exp, reply_id, caller)
     flag, to_find, replace = exp.split('/')
@@ -12,7 +13,7 @@ class ModifyMessageJob < ApplicationJob
 
     final_text = initial_text.gsub(to_find_reg, replace)
     final_text = "#{final_text[0..197]}..." if final_text.length > 200
-    SendMessageJob.perform_later(send_to, @fail_replies.sample, caller) if final_text == initial_text
+    SendMessageJob.perform_later(send_to, fail_replies.sample, caller) if final_text == initial_text
     SendMessageJob.perform_later(send_to, final_text, reply_id)
   end
 end
