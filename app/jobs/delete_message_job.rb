f7b99c7d2e989d914d6a3d2f241a2a5b8f6ab408
Aram <aramhrptn@hotmail.com>
2021-04-28 21:49:35 +0700

    added remove initial message functionality

--- app/jobs/delete_message_job.rb
+++ app/jobs/delete_message_job.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-class SendMessageJob < ApplicationJob
+class DeleteMessageJob < ApplicationJob
   queue_as :default
 
-  def perform(chat_id, text, reply_to_message_id)
+  def perform(chat_id, message_id)
     bot_token = ENV.fetch('BOT_TOKEN') { Rails.application.credentials.tg[:token] }
     token = "bot#{bot_token}"
     base_url = "https://api.telegram.org/bot#{token}"
     connection = Faraday.new(base_url)
-    url = "/bot#{bot_token}/sendMessage"
-    body = { chat_id: chat_id, text: text, reply_to_message_id: reply_to_message_id, parse_mode: 'MarkdownV2' }
+    url = "/bot#{bot_token}/deleteMessage"
+    body = { chat_id: chat_id, message_id: message_id }
     req = connection.post do |m|
       m.url(url)
       m.headers['Content-Type'] = 'application/json'
--- app/jobs/modify_message_job.rb
+++ app/jobs/modify_message_job.rb
@@ -6,7 +6,7 @@ class ModifyMessageJob < ApplicationJob
   def fail_replies = ['ты че ебанутый?', 'сам сказал что понял?', 'тупа не осилил регулярки',
                       'тебе с такими регулярками не стыдно людям в глаза смотреть?']
 
-  def perform(_initial_message_id, send_to, initial_text, exp, reply_id, caller)
+  def perform(initial_message_id, send_to, initial_text, exp, reply_id, caller)
     flag, to_find, replace = exp.split('/')
 
     to_find_reg = Regexp.new(to_find)
@@ -15,6 +15,7 @@ class ModifyMessageJob < ApplicationJob
     final_text = "#{final_text[0..197]}..." if final_text.length > 200
     return SendMessageJob.perform_later(send_to, fail_replies.sample, caller) if final_text == initial_text
 
+    DeleteMessageJob.perform_later(send_to, initial_message_id) if flag[0] == '-'
     SendMessageJob.perform_later(send_to, final_text, reply_id)
   end
 end
