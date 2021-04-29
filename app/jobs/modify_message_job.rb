5211707f1c901df2930292d4e3dbd458a04be546
urumo <aramhrptn@hotmail.com>
2021-04-29 22:06:58 +0700

    fixed output errors if env is prod

--- app/jobs/modify_message_job.rb
+++ app/jobs/modify_message_job.rb
@@ -35,7 +35,7 @@ class ModifyMessageJob < ApplicationJob
     DeleteMessageJob.perform_later(send_to, initial_message_id) if flag[0] == ('-') && (final_text != initial_text)
     SendMessageJob.perform_later(send_to, Regexp.escape(final_text), reply_id)
   rescue StandardError => e
-    SendMessageJob.perform_later(send_to, Regexp.escape(e.message), caller)
+    SendMessageJob.perform_later(send_to, Regexp.escape(e.message), caller) unless Rails.env == "production"
   end
 
   private
