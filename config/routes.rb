46887878f81597b194d992d12653eea8d24814b2
Aram <aramhrptn@hotmail.com>
2021-04-29 23:07:26 +0700

    added rey controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'rey/index'
   root 'home#index'
   post 'telegram/get_updates'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
