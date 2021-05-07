f1471de16c89d76addb91fbaa51343afb477a6ae
urumo <aramhrptn@hotmail.com>
2021-05-08 03:11:07 +0700

    refactor and bug fixes (#11)

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Bot < ApplicationRecord
-  def send_message(chat_id, message, reply_id) = SendMessageJob.perform_later(chat_id, Regexp.escape(message),
-                                                                              reply_id, token)
+  def send_message(chat_id, message, reply_id, sleep_time = 0) = SendMessageJob
+    .set(wait: sleep_time.seconds)
+    .perform_later(chat_id, Regexp.escape(message), reply_id, token)
 
   def delete_message(chat_id, message_id) = DeleteMessageJob.perform_later(chat_id, message_id, token)
 end
