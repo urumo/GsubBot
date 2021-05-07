66607c7b717458b07ffde1b12a0a87f0cc9ebdf3
aram <aramhrptn@hotmail.com>
2021-05-08 04:05:51 +0700

    fix: fixed ActiveRecord::RecordNotUnique

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,9 @@ class User < ApplicationRecord
     caller.admin
   end
 
-  def self.get_user(target, chat_id) = User.find_or_create_by(tg_id: target.id, user_name: target.username,
-                                                              chat_id: chat_id)
+  def self.get_user(target, chat_id)
+    User.find_or_create_by(tg_id: target.id, user_name: target.username, chat_id: chat_id)
+  rescue ActiveRecord::RecordNotUnique
+    User.find_by(tg_id: target.id, user_name: target.username, chat_id: chat_id)
+  end
 end
